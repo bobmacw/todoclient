437ffb0519064e6171273e485c2b036fa5f77be8
bobmacw <bobmacw@yahoo.com>
2015-05-15 06:53:59 -0400

    first pass at comment structure

--- tutorial1.js
+++ tutorial1.js
@@ -2,12 +2,35 @@
  * Created by ubuntu on 3/4/15.
  */
 
-// tutorial1.js
+// tutorial1
+var CommentList = React.createClass({
+    render: function() {
+        return (
+            <div className="commentList">
+                Hello, React world - I am a CommentList!
+            </div>
+        );
+    }
+})
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+                Hello, React world - I am a CommentForm!
+            </div>
+        );
+    }
+})
+
+
 var CommentBox = React.createClass({
     render: function() {
         return (
             <div className="commentBox">
-                Hello, world! I am a CommentBox.
+                <h1> -- COMMENTS -- </h1>
+                <CommentList />
+                <CommentForm />
             </div>
         );
     }
